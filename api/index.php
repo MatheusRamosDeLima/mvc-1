dd2a55a6d48ac74b2de37f5a9a9854ea171b75da
MatheusRamosDeLima <matheusramosej@gmail.com>
2024-05-06 22:51:48 -0300

    Way modified to require files in api/index.php to works in Vercel

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once "../vendor/autoload.php";
-require_once "../app/routes/main.php";
+require_once __DIR__."/../vendor/autoload.php";
+require_once __DIR__."/../app/routes/main.php";
 
 use App\Core\Core;
 use App\Http\Route;
