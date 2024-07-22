e87fdfe44c17008d24dd0cb2d107c2c2edc15332
MatheusRamosDeLima <matheusramosej@gmail.com>
2024-07-22 18:40:17 -0300

    View: name attribute renamed to path

--- app/Utils/View.php
+++ app/Utils/View.php
@@ -3,18 +3,18 @@
 namespace App\Utils;
 
 class View {
-    private string $name, $title;
+    private string $path, $title;
     private string|array|null $css = null, $js = null;
 
-    public function __construct(string $name, string $title, string|array|null $css = null, string|array|null $js = null) {
-        $this->name = $name;
+    public function __construct(string $path, string $title, string|array|null $css = null, string|array|null $js = null) {
+        $this->path = $path;
         $this->title = $title;
         $this->css = $css;
         $this->js = $js;
     }
 
-    public function getName():string {
-        return $this->name;
+    public function getPath():string {
+        return $this->path;
     }
     public function getTitle():string {
         return $this->title;
